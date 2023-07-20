a7b0c44d0d1d75727d003a665305a6af5e23c06c
Лазарев Дмитрий <69201114+lazarenok312@users.noreply.github.com>
2023-07-20 14:35:46 +0600

    reset password

--- account/views.py
+++ account/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import render, redirect
 
 def user_login(request):
     if request.method == 'POST':
@@ -20,4 +23,17 @@ def user_login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+    return render(request, 'registration/login.html', {'form': form})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Обновление сессии для избежания разлогинивания
+            return redirect('password_change_done')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password_change_form.html', {'form': form})
--- exobed/settings.py
+++ exobed/settings.py
@@ -125,8 +125,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # LOGIN_REDIRECT_URL = '/sensor_list/'
-# LOGOUT_REDIRECT_URL = '/sensor_list/'
+# LOGOUT_REDIRECT_URL = '/login/'
 # LOGIN_URL = reverse_lazy('login')
 # LOGOUT_URL = reverse_lazy('logout')
