dae4ed7a647f8a131cc8e8643b488bd9ab30570c
Лазарев Дмитрий <69201114+lazarenok312@users.noreply.github.com>
2023-07-26 17:32:04 +0600

    add paginate

--- esp_sensor/views.py
+++ esp_sensor/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
 from .models import Sensor
-from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 class SensorListView(ListView):
@@ -34,3 +36,28 @@ def search_sensors(request):
         sensors = Sensor.objects.all()
 
     return render(request, 'sensor/search_results.html', {'sensors': sensors, 'query': query})
+
+
+@csrf_exempt
+def update_sensor(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            sensor_id = data.get('id')
+            if sensor_id is not None:
+                sensor = Sensor.objects.get(pk=sensor_id)
+                sensor.name = data.get('name', sensor.name)
+                sensor.slug = data.get('slug', sensor.slug)
+                sensor.description = data.get('description', sensor.description)
+                sensor.owner = data.get('owner', sensor.owner)
+                sensor.inclusions = data.get('inclusions', sensor.inclusions)
+                sensor.power = data.get('power', sensor.power)
+                sensor.work = data.get('work', sensor.work)
+                sensor.save()
+                return JsonResponse({'success': True, 'message': 'Sensor updated successfully.'})
+            else:
+                return JsonResponse({'success': False, 'message': 'Sensor ID not provided.'})
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': str(e)})
+    else:
+        return JsonResponse({'success': False, 'message': 'Invalid request method.'})
